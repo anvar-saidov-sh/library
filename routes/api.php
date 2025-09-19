290880790d70a921f0e97d5d276d58cdffa69790
anvarbeck <saidovanvar862@gmail.com>
2025-09-19 15:47:07 +0500

    Controller Work

---
+++ routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Http\Controllers\AuthorController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| These routes are loaded by the RouteServiceProvider and are automatically
+| assigned the "api" middleware group. All routes here will be prefixed with /api.
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::apiResource('authors', AuthorController::class);
