cb429370120aeba495e445c47df57189dd58d6a8
anvarbeck <saidovanvar862@gmail.com>
2025-09-20 17:31:16 +0500

    FRONTEND

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
@@ -20,4 +21,5 @@ Route::middleware('auth')->group(function () {
 Route::middleware(['auth'])->group(function () {
     Route::resource('books', BookController::class);
 });
+Route::resource('authors', AuthorController::class);
 require __DIR__.'/auth.php';
